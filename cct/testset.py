fd5756b15b5d64f8692a3b38d72e71e4bfe20c65
李松 <mango@192.168.0.102>
2018-10-21 01:00:31 +0800

    delete unused testset variable

--- cct/testset.py
+++ cct/testset.py
@@ -2,7 +2,6 @@ class TestSet(object):
 
     def __init__(self, case_dict):
         self.__case_dict = case_dict
-        self.__case_running_status_dict = {}
         self.__case_dep_running_status_dict = {}
         self.__case_dep_op_status_dict = {}
         self.__load_data()
@@ -15,30 +14,10 @@ class TestSet(object):
     def case_dict(self):
         return self.__case_dict
 
-    def __load_case_dependence_relation(self):
-        '''
-        add caseId_status: case_list to local dict
-        :return:
-        '''
-        for case_id, case in self.case_dict.items():
-            depend_case_id = case.dependence.get('id')
-            op_dep_case_id = case.__dependence_process_at.get('id')
-            op_dep_status = case.__dependence_op('status')
-            key = '{case_id}_{status}'.format(case_id=depend_case_id,
-                                              status=case.dependence.get('status').upper())
-            key_operation = '{case_id}_{status}'.format(case_id=op_dep_case_id,
-                                                        status=op_dep_status.upper())
 
-            case_list = self.__case_dep_running_status_dict.get(
-                key) if key in self.__case_dep_running_status_dict else []
-            case_list.append(case)
-            if key in self.__case_dep_running_status_dict:
-                self.__case_dep_running_status_dict[key] = case_list
-
-            case_op_dep_list = self
 
     def __load_data(self, is_dep_running=True):
-        target_dict = self.__case_running_status_dict if is_dep_running else self.__case_dep_op_status_dict
+        target_dict = self.__case_dep_running_status_dict if is_dep_running else self.__case_dep_op_status_dict
         for id, case in self.case_dict.items():
             dep_dict = case.dependence if is_dep_running else case.dep_op_dict
             if dep_dict is not None:
@@ -63,7 +42,7 @@ class TestSet(object):
         # make the related case available
         key = '{case_id}_{status}'.format(case_id=case.case_id, status=case_status.key.upper())
 
-        cases = self.__case_running_status_dict.get(key)
+        cases = self.__case_dep_running_status_dict.get(key)
         if cases:
             for dep_case in cases:
                 dep_case.resume()
